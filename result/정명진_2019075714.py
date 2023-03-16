b7b48f7e0c6263546853566c240ed0e59ae2d047
homework01 <audwls7573034@naver.com>
2023-03-16 12:15:35 +0900

    정명진_2019075714

--- result/정명진_2019075714.py
+++ result/정명진_2019075714.py
@@ -9,12 +9,12 @@ if int(year)==9:
     year='19'+p_number[0:2]
 else:
     year='20'+p_number[0:2]
-month=p_number[2:4]
+month=p_number[2:4].lstrip('0')
 day=p_number[4:6]
 if int(p_number[7]) in [1,3]:
     gender='남자'
 else:
     gender='여자'
-print('나는 {}년 {}월 {}일에 태어난 {}입니다.'.format(year,month,day,gender))
+print('나는 {}년 {}월 {}일에 태어난 {}입니다.'.format(year,month,day,gender),end='')
 #############################################################
 #파일 이름은 자신의 학번, 이름으로 설정해주세요.
