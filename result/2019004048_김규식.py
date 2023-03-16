3ef569778fcf3087391aed2e8192c81286f909c6
김규식 <kimq6@hanyang.ac.kr>
2023-03-16 10:55:36 +0900

    주민등록번호 입력시 생년월일과 성별을 출력

---
+++ result/2019004048_김규식.py
@@ -0,0 +1,35 @@
+# 학번 2019004048
+# 이름 김규식
+
+
+#############################################################
+# 코드
+# 입력 받기
+number = input("주민등록 번호를 입력하세요")
+# 슬라이싱 후 정수 변환
+number_year = int(number[0:2])
+number_month = int(number[2:4])
+number_day = int(number[4:6])
+number_gender = int(number[7])
+
+# 출력시 달라지는 부분
+if number_gender == 1:
+    gender = "남자"
+    number_year += 1900
+if number_gender == 2:
+    gender = "여자"
+    number_year += 1900
+if number_gender == 3:
+    gender = "남자"
+    number_year += 2000
+if number_gender == 4:
+    gender = "여자"
+    number_year += 2000
+
+#출력
+print("나는 {}년 {}월 {}일에 태어난 {}입니다.".format(number_year, number_month, number_day, gender))
+
+#############################################################
+
+
+#파일 이름은 자신의 학번, 이름으로 설정해주세요.
